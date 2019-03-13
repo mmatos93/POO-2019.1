7434aeb71a0acb3666ee266f394328f269494f73
mmatos93 <mthsmts@gmail.com>
2019-03-12 23:03:14 -0300

    Primeira parte da Lista01, ainda falta fazer o resto dos exercicios

--- Lista01/Lista01-06.java
+++ Lista01/Lista01-06.java
@@ -1,20 +1,27 @@
 import java.util.Scanner;
 
-public class Conceito{
+public class Hotel{
 
 	public static void main(String[] args){
 
 		Scanner s = new Scanner(System.in);
-		int nota;
-		String[] conceito = {"Insuficiente", "Regular", "Bom", "Otimo"};
-		System.out.print("Digite a nota do aluno: ");
-		nota = s.nextInt();
+		String nome;
+		int diarias;
+		System.out.print("Digite o nome do cliente: ");
+		nome = s.nextLine();
+		System.out.print("Digite a quantidade de diarias: ");
+		diarias = s.nextInt();
 
-		if(nota >= 0 && nota < 50) System.out.println("Conceito: " + conceito[0]);
-		else if(nota >= 50 && nota < 65) System.out.println("Conceito: " + conceito[1]);
-		else if(nota >= 65 && nota < 85) System.out.println("Conceito: " + conceito[2]);
-		else if(nota >= 85 && nota <= 100) System.out.println("Conceito: " + conceito[3]);
-		else System.out.println("Valor invalido digitado");
+		if(diarias > 15){
+			System.out.println("Cliente: " + nome + " , total da conta: R$ " + (60*diarias+5.50));
+		}
+		else if(diarias == 15){
+			System.out.println("Cliente: " + nome + " , total da conta: R$ " + (60*diarias+6.0));
+		}
+		else {
+			System.out.println("Cliente: " + nome + " , total da conta: R$ " + (60*diarias+8.0));
+		}
+
 	}
 
 }
